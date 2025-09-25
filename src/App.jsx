e162cb9f161e49834f39c2f5502d1ee10d2ad1c0
Anjali-pandey11 <pandeyanjali7906@gmail.com>
2025-09-25 13:47:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Heading from "./components/Heading";
+import Slogan from "./components/Slogan";
+import TimeDisplay from "./components/TimeDisplay";
+import "./Bharat.css";
+
+function App(){
+  return <center>
+    <Heading></Heading>
+    <Slogan></Slogan>
+    <TimeDisplay></TimeDisplay>
+   
+  </center>
+ 
+}
+
+
+export default App;
